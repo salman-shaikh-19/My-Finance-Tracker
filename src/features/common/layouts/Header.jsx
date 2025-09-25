1abd1098f887215474d038011a28f20e87731c5c
salman shaikh <salman.shaikh@differenz.com>
2025-09-25 18:43:38 +0530

    ui

--- src/features/common/layouts/Header.jsx
+++ src/features/common/layouts/Header.jsx
@@ -1,80 +1,78 @@
 import { useDispatch } from "react-redux";
 import { logoutUser } from "../../auth/authSlice";
 import ThemeToggle from "../components/ThemeToggle";
-import { MdHome } from "react-icons/md";
+import { MdHome, MdMoreHoriz } from "react-icons/md";
 import { BiSearch } from "react-icons/bi";
 
 const Header = () => {
   const dispatch = useDispatch();
-  //   const navigate = useNavigate();
 
   const handleLogout = async () => {
     try {
       await dispatch(logoutUser()).unwrap();
-      //   navigate("/login"); // redirect to login after logout
     } catch (err) {
       console.error("Logout failed:", err);
     }
   };
+
   return (
     <>
+      {/* Desktop Navbar */}
       <nav className="hidden md:flex fixed top-0 w-full bg-base-100 text-base-content h-16 items-center justify-around shadow-md">
-        <a href="#" className="px-4 hover:text-primary">
-          Home
-        </a>
-        <a href="#" className="px-4 hover:text-primary">
-          Search
-        </a>
-        <a href="#" className="px-4 hover:text-primary">
-          Reels
-        </a>
-        <a href="#" className="px-4 hover:text-primary">
-          Activity
-        </a>
+        <a href="#" className="px-4 hover:text-primary">Home</a>
+        <a href="#" className="px-4 hover:text-primary">Search</a>
+        <a href="#" className="px-4 hover:text-primary">Reels</a>
+        <a href="#" className="px-4 hover:text-primary">Activity</a>
         <ThemeToggle />
-        <button onClick={handleLogout} className="btn btn-primary ">
-          Logout
-        </button>
+        <button onClick={handleLogout} className="btn btn-primary">Logout</button>
       </nav>
 
-      {/* Mobile/Tablet Navbar */}
+      {/* Mobile Navbar */}
       <nav className="md:hidden fixed bottom-4 left-1/2 transform -translate-x-1/2 w-11/12 bg-base-100 text-base-content shadow-lg rounded-2xl">
-        <div className="flex justify-around items-center h-16 px-4">
-          <a
-            href="#"
-            className="flex flex-col items-center justify-center hover:text-primary group"
-          >
-            <MdHome />
+        <div className="flex justify-around items-center h-16 px-2">
+          {/* Home */}
+          <a href="#" className="flex flex-col items-center justify-center hover:text-primary group">
+            <MdHome size={24} />
             <span className="text-xs">Home</span>
-            <span className="block w-5 h-1 rounded-full bg-primary hidden group-hover:block"></span>
           </a>
-          <a
-            href="#"
-            className="flex flex-col items-center justify-center hover:text-primary group"
-          >
-            <BiSearch />
+
+          {/* Explore */}
+          <a href="#" className="flex flex-col items-center justify-center hover:text-primary group">
+            <BiSearch size={24} />
             <span className="text-xs">Explore</span>
-            <span className="block w-5 h-1 rounded-full bg-primary hidden group-hover:block"></span>
           </a>
-          <a
-            href="#"
-            className="flex flex-col items-center justify-center hover:text-primary group"
-          >
-            <span className="text-xs">Reels</span>
-            <span className="block w-5 h-1 rounded-full bg-primary hidden group-hover:block"></span>
-          </a>
-          <a
-            href="#"
-            className="flex flex-col items-center justify-center hover:text-primary group"
-          >
-            <i className="far fa-heart text-2xl mb-1 text-current"></i>
-            <span className="text-xs">Activity</span>
-            <span className="block w-5 h-1 rounded-full bg-primary hidden group-hover:block"></span>
-          </a>
-           <ThemeToggle />
-        <button onClick={handleLogout} className="btn btn-primary ">
-          Logout
-        </button>
+
+          {/* Dropdown for extra items */}
+    <div className="dropdown dropdown-top dropdown-end">
+  {/* Trigger */}
+  <label
+    tabIndex={0}
+    className="flex flex-col items-center justify-center cursor-pointer hover:text-primary focus:outline-none"
+  >
+    <MdMoreHoriz size={24} />
+    <span className="text-xs">More</span>
+  </label>
+
+  {/* Dropdown content */}
+  <ul
+    tabIndex={0}
+    className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-40 max-h-60 overflow-auto"
+  >
+    <li>
+      <a href="#">Reels</a>
+    </li>
+    <li>
+      <a href="#">Activity</a>
+    </li>
+    <li>
+      <button onClick={handleLogout}>Logout</button>
+    </li>
+    <li>
+      <ThemeToggle />
+    </li>
+  </ul>
+</div>
+
         </div>
       </nav>
     </>
