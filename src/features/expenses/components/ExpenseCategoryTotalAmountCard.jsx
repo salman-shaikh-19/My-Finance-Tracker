cca2bbf4bf0b3f3e3acdfe771aab318c5445c732
salman shaikh <salman.shaikh@differenz.com>
2025-10-01 18:37:25 +0530

    lts

--- src/features/expenses/components/ExpenseCategoryTotalAmountCard.jsx
+++ src/features/expenses/components/ExpenseCategoryTotalAmountCard.jsx
@@ -1,23 +1,21 @@
 import React, { useMemo } from "react";
 import { formatCurrency } from "../../../utils/currencyUtils";
 
-const ExpenseCategoryTotalAmountCard=({ 
-  name, 
-  totalAmount = 0, 
-  Icon, 
-  bg, 
+const ExpenseCategoryTotalAmountCard = ({
+  name,
+  totalAmount = 0,
+  Icon,
+  bg,
   userCurrency = "INR",
-  theme 
-})=>{
-   const formattedCurrency = useMemo(
-     () => formatCurrency(totalAmount, userCurrency),
-     [totalAmount, userCurrency]
-   );
+  theme,
+}) => {
+  const formattedCurrency = useMemo(
+    () => formatCurrency(totalAmount, userCurrency),
+    [totalAmount, userCurrency]
+  );
   return (
     <div
-   title={`You have spent ${formattedCurrency} on ${name}`}
-
-
+      title={`You have spent ${formattedCurrency} on ${name}`}
       className="flex w-40 items-center bg-base-100  transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl flex-col gap-2 card shadow-lg rounded-lg p-4"
     >
       <div
@@ -29,18 +27,13 @@ const ExpenseCategoryTotalAmountCard=({
           }`}
         />
       </div>
-         <p className="mt-3 text-lg font-semibold  text-center">
-        {name}
-      </p>
+      <p className="mt-3 text-lg font-semibold  text-center">{name}</p>
 
       {/* Expense Count */}
       <span className="text-sm font-medium text-gray-500 ">
-       {formattedCurrency}
+        {formattedCurrency}
       </span>
     </div>
   );
-
-
-
-}
+};
 export default React.memo(ExpenseCategoryTotalAmountCard);
