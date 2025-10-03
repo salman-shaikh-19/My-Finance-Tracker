14503ec28eee7a96db1c1e7c1a2bf665bbf2c496
salman shaikh <salman.shaikh@differenz.com>
2025-10-03 15:37:02 +0530

    lts

--- src/features/common/components/PrevNextButton.jsx
+++ src/features/common/components/PrevNextButton.jsx
@@ -50,7 +50,7 @@ const PrevNextButton = ({
         </button>
         <button
           title={`Refresh data of ${weekLabel} `}
-          className="btn btn-accent btn-outline btn-sm disabled:cursor-not-allowed "
+          className="btn btn-info btn-outline btn-sm disabled:cursor-not-allowed "
           onClick={handleRefresh}
           // disabled={} // disable
         >
