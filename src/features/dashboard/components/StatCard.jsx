784a63aaace01d6d9e9c338c2fe7a7bc8f4fdd0e
salman shaikh <salman.shaikh@differenz.com>
2025-10-15 15:33:13 +0530

    lts

--- src/features/dashboard/components/StatCard.jsx
+++ src/features/dashboard/components/StatCard.jsx
@@ -1,19 +1,11 @@
 import React from "react";
 import Skeleton from "react-loading-skeleton";
 import 'react-loading-skeleton/dist/skeleton.css';
+import CardSkeleton from "../../common/components/CardSkeleton";
 const StatCard = ({ cardTitle, cardTotal, loading,additionalClass='' }) => {
   return (
     <>
-      {loading ? <div className={`card w-full bg-base-200 shadow-xl ${additionalClass}`}>
-        <div className="card-body flex flex-col items-center justify-center gap-4">
-          <div className="flex items-center gap-4 w-full">
-            <div className="flex flex-col gap-2 flex-1">
-              <div className="skeleton h-4 w-45 rounded bg-base-100"></div>
-              <div className="skeleton h-4 w-auto rounded bg-base-100"></div>
-            </div>
-          </div>
-        </div>
-      </div> : (
+      {loading ? <CardSkeleton additionalClass={additionalClass} /> : (
         <div className={`card w-full bg-base-200 shadow-xl ${additionalClass}`}>
           <div className="card-body">
             <h2 className="card-title">{cardTitle}</h2>
